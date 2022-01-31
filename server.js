fb4f7906f602d90d82b61d1c0f7efa668cfc785b
Andy Terwilliger <andy@terwilliger1@gmail.com>
2022-01-31 10:59:53 -0600

    firebase

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const admin = require("firebase-admin");
+
+
 
 // initialize the express app
 const app = express();
@@ -11,7 +14,15 @@ const app = express();
 require('dotenv').config();
 
 // expose our config variables
-const { MONGODB_URL, PORT = 4000 } = process.env;
+const { MONGODB_URL, PORT = 4000, GOOGLE_CREDENTIALS } = process.env;
+
+const serviceAccount = JSON.parse(GOOGLE_CREDENTIALS);
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 // connect to mongoDB
 mongoose.connect(MONGODB_URL);
@@ -28,7 +39,8 @@ db
 const peopleSchema = new mongoose.Schema({
     name: String,
     image: String,
-    title: String
+    title: String,
+    uid: String
 }, { timestamps: true });
 
 const People = mongoose.model('People', peopleSchema);
@@ -37,6 +49,25 @@ const People = mongoose.model('People', peopleSchema);
 app.use(express.json()); // this creates req.body using incoming JSON from our requests
 app.use(morgan('dev'));
 app.use(cors());
+app.use(async function(req, res, next) {
+    try{
+    const token = req.get('Authorization');
+    if(!token) return next();
+    
+    const user = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
+    if(!user) throw new Error('Something went wrong...')
+
+    req.user = user;
+    next();
+    } catch (error){
+        res.status(400).json(error);
+    }
+})
+
+function isAuthenticated(req, res, next){
+    if(!req.user) return res.status(401).json({message:'You must be logged in first.'})
+    next(); 
+}
 
 // routes
 app.get('/', (req, res) => {
@@ -44,17 +75,19 @@ app.get('/', (req, res) => {
 });
 
 // index route
-app.get('/people', async (req, res) => {
+app.get('/people', isAuthenticated, async (req, res) => {
     try {
-        res.json(await People.find({}));
+        res.json(await People.find({ uid: req.user.uid }));
     } catch (error) {
         res.status(400).json(error);
     }
 });
 
 // create route
-app.post('/people', async (req, res) => {
+app.post('/people', isAuthenticated, async (req, res) => {
+    
     try {
+        req.body.uid = req.user.uid;
         res.json(await People.create(req.body));
     } catch (error) {
         res.status(400).json(error);
